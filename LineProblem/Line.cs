fb6419b6307217a409c9b1660d5670df7d13c115
R.K Nanda <rk@RK-s-MacBook-Air.local>
2023-07-07 10:41:43 +0530

    [RK Nanda] Adding a class and constructor to calculate the length of a line UC1

---
+++ LineProblem/Line.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace LineProblem
+{
+	public class Line
+	{
+		private double x1, x2, y1, y2;
+
+		//parameterized constructor
+		public Line(double a ,double b, double c, double d)
+		{
+			this.x1 = a;
+            this.y1 = b;
+            this.x2 = c;
+            this.y2 = d;
+
+        }
+
+        //Default Constructor
+        public Line()
+        {
+            this.x1 = 0;
+            this.y1 = 0;
+            this.x2 = 0;
+            this.y2 = 0;
+
+        }
+
+        //Calculates length of a line
+        public double Length()
+        {
+            double x = this.x2 - this.x1;
+            double y = this.y2 - this.y1;
+
+            double length;
+            length = Math.Sqrt((x * x + y * y));
+            return length;
+        }
+
+
+    }
+}
+
--- LineProblem/Program.cs
+++ LineProblem/Program.cs
@@ -1,8 +1,25 @@
-﻿internal class Program
-{
-    private static void Main(string[] args)
+﻿using System;
+namespace LineProblem {
+
+    internal class Program
     {
-        //Start
-        Console.WriteLine("Welcome to Line Comparison Computation");
+        private static void Main(string[] args)
+        {
+            //Start
+            Console.WriteLine("Welcome to Line Comparison Computation");
+
+            //UC1
+            Console.WriteLine("---UC1---");
+            Console.WriteLine("Enter Coordinates of line");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            Line l1 = new Line(x1,y1,x2,y2);
+            double length1 = l1.Length();
+            Console.WriteLine($"The length of line is {length1}");
+
+        }
     }
+
 }
